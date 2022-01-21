558e9ce42d98040f98f90eca37f5ec580d927931
PinkR1ver <3180102330@zju.edu.cn>
2022-01-21 18:26:49 +0800

    Update gene expression monitor

--- gene_expression_monitor.py
+++ gene_expression_monitor.py
@@ -1,10 +1,14 @@
 import os
+from re import L
+from turtle import title
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from rich import print
 import gc
+
+from torch import FloatStorage
 
 basePath = r''
 dataPath = os.path.join(basePath, 'data')
@@ -14,60 +18,119 @@ geneFile = 'gene_expression_details.csv'
 
 if __name__ == '__main__':
     gene_expression_details = pd.read_csv(os.path.join(dataPath, geneFile))
+    tumor_regions = gene_expression_details.columns[18:-1]
+
     
     # Get tumor_name
     tumor_name = gene_expression_details['tumor_name']
     tumor_name = tumor_name.drop_duplicates()
     tumor_name = tumor_name.reset_index(drop=True)
 
-    for i in tumor_name:
+    for tumor_region in tumor_regions:
 
-        # Get sub group
-        sub_gene_expression_details = gene_expression_details.loc[gene_expression_details['tumor_name'] == i]
-        sub_gene_expression_details = sub_gene_expression_details.reset_index(drop=True)
-        ## print(sub_gene_expression_details)
+        for i in tumor_name:
 
-        # Get gene symbol from subgroup
-        gene_symbol = sub_gene_expression_details['gene_symbol']
-        gene_symbol = gene_symbol.drop_duplicates()
-        gene_symbol = gene_symbol.reset_index(drop=True)
-        ## print(gene_symbol)
+            if os.path.isfile(os.path.join(savePath, tumor_region, i, 'log.txt')):
+                f = open(os.path.join(savePath, tumor_region, i, 'log.txt'), "w")
+                f.close()
+            if os.path.isfile(os.path.join(savePath, tumor_region, i, 'loss.txt')):
+                f = open(os.path.join(savePath, tumor_region, i, 'loss.txt'), "w")
+                f.close()
 
+            # Get sub group
+            sub_gene_expression_details = gene_expression_details.loc[gene_expression_details['tumor_name'] == i]
+            sub_gene_expression_details = sub_gene_expression_details.reset_index(drop=True)
+            ## print(sub_gene_expression_details)
 
-        # Extract one tumor, one gene
-        for j in gene_symbol:
-            sub_sub_gene_expression_details = sub_gene_expression_details.loc[sub_gene_expression_details['gene_symbol'] == j]
-            expression_energy_le = sub_sub_gene_expression_details['expression_energy_le']
-            expression_energy_le = expression_energy_le.reset_index(drop=True)
-            expression_energy_it = sub_sub_gene_expression_details['expression_energy_it']
-            expression_energy_it = expression_energy_it.reset_index(drop=True)
-            if len(expression_energy_le[~(expression_energy_le.isnull())]) > 10:
-                fig = plt.figure(figsize=(6, 6))
-                if not os.path.isdir(os.path.join(savePath, 'expression_energy_le', i)):
-                    os.mkdir(os.path.join(savePath, 'expression_energy_le', i))
-                print(j)
-                print(expression_energy_le)
-                sns.kdeplot(sub_sub_gene_expression_details['expression_energy_le'])
-                plt.savefig(os.path.join(savePath, 'expression_energy_le', i, j+'.png'))
-                plt.close(fig)
-                gc.collect()
-            else:
-                plt.close(fig)
-                gc.collect()
-            
-            if len(expression_energy_it[~(expression_energy_it.isnull())]) > 10:
-                fig = plt.figure(figsize=(6, 6))
-                if not os.path.isdir(os.path.join(savePath, 'expression_energy_it', i)):
-                    os.mkdir(os.path.join(savePath, 'expression_energy_it', i))
-                print(j)
-                print(expression_energy_it)
-                sns.kdeplot(sub_sub_gene_expression_details['expression_energy_it'])
-                plt.savefig(os.path.join(savePath, 'expression_energy_it', i, j+'.png'))
-                plt.close(fig)
-                gc.collect()
-            else:
-                plt.close(fig)
-                gc.collect()
+            # Get gene symbol from subgroup
+            gene_symbol = sub_gene_expression_details['gene_symbol']
+            gene_symbol = gene_symbol.drop_duplicates()
+            gene_symbol = gene_symbol.reset_index(drop=True)
+            ## print(gene_symbol)
+
+            # Extract one tumor, one gene
+
+            fig_all = plt.figure(num="Together", figsize=(30, 30))
+
+            color_may = len(gene_symbol)
+
+            labels = []
+
+            for flag, j in enumerate(gene_symbol):
 
 
+                sub_sub_gene_expression_details = sub_gene_expression_details.loc[sub_gene_expression_details['gene_symbol'] == j]
+                
+                specific_tumor_region_expression_energy = sub_sub_gene_expression_details[tumor_region]
+                specific_tumor_region_expression_energy = specific_tumor_region_expression_energy.reset_index(drop=True)
+                remove_null_specific_tumor_region_expression_energy = specific_tumor_region_expression_energy[~(specific_tumor_region_expression_energy.isnull())]
+                remove_null_specific_tumor_region_expression_energy = remove_null_specific_tumor_region_expression_energy.reset_index(drop=True)
+                sort_specific_tumor_region_expression_energy = np.sort(remove_null_specific_tumor_region_expression_energy.to_numpy())
 
+
+                if len(remove_null_specific_tumor_region_expression_energy) > 8:
+                    fig = plt.figure(num="Current", figsize=(6, 6))
+
+                    if not os.path.isdir(os.path.join(savePath, tumor_region)):
+                        os.mkdir(os.path.join(savePath, tumor_region))
+                    if not os.path.isdir(os.path.join(savePath, tumor_region, i)):
+                        os.mkdir(os.path.join(savePath, tumor_region, i))
+                    if not os.path.isfile(os.path.join(savePath, tumor_region, i, 'log.txt')):
+                        f = open(os.path.join(savePath, tumor_region, i, 'log.txt'), "x")
+                        f.close()
+
+                    print(j)
+                    print(remove_null_specific_tumor_region_expression_energy)
+
+                    sns.histplot(remove_null_specific_tumor_region_expression_energy, stat='density', kde=True)
+                    plt.title(j)
+                    plt.savefig(os.path.join(savePath, tumor_region, i, j+'.png'))
+                    plt.close(fig)
+
+                    plt.figure("Together")
+                    sns.scatterplot(x=flag ,y=remove_null_specific_tumor_region_expression_energy.values, size=1000 ,color=(sns.color_palette("hls", color_may))[flag])
+                    labels.append(j)
+                    
+                    f = open(os.path.join(savePath, tumor_region, i, 'log.txt'), "a")
+                    f.write(f'{j}:\n')
+                    for k in sort_specific_tumor_region_expression_energy:
+                        f.write(f'{str(k)}\n')
+                    f.write('---------------------------------------------------\n\n\n')
+                    f.close()
+                    gc.collect()
+                
+                else:
+
+                    print(j)
+
+                    if len(remove_null_specific_tumor_region_expression_energy) >= 1:
+                        plt.figure("Together")
+                        sns.scatterplot(x=flag ,y=remove_null_specific_tumor_region_expression_energy.values, size=1000 ,color=(sns.color_palette("hls", color_may))[flag])
+                        labels.append(j)
+
+                    if not os.path.isdir(os.path.join(savePath, tumor_region)):
+                        os.mkdir(os.path.join(savePath, tumor_region))
+                    if not os.path.isdir(os.path.join(savePath, tumor_region, i)):
+                        os.mkdir(os.path.join(savePath, tumor_region, i))
+                    if not os.path.isfile(os.path.join(savePath, tumor_region, i, 'loss.txt')):
+                        f = open(os.path.join(savePath, tumor_region, i, 'loss.txt'), "x")
+                        f.close()
+                    
+                    f = open(os.path.join(savePath, tumor_region, i, 'loss.txt'), "a")
+                    f.write(f'{j}:\n')
+                    for k in sort_specific_tumor_region_expression_energy:
+                        f.write(f'{str(k)}\n')
+                    f.write('---------------------------------------------------\n\n\n')
+                    f.close()
+
+                    plt.close(fig)
+                    gc.collect()
+
+            plt.figure("Together")
+            plt.legend(title='Gene', loc='upper right', labels=labels, ncol=6)
+            plt.title("Compare different gene expression energy in " + tumor_region)
+            plt.xlabel("Different Genes")
+            plt.ylabel("Expression Energy")
+            plt.savefig(os.path.join(savePath, tumor_region, i, 'ALL.png'))
+            plt.close(fig_all)
+            gc.collect()
